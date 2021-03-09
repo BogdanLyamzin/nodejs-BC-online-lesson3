56932e16cb834a60f15951a18a7c42d1f08000d0
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-03-09 21:42:13 +0200

    finish lesson

--- example-2.5/app.js
+++ example-2.5/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-// app.use(express.json());
-
-const bodyJSON = express.json();
+const bodyJSON = bodyParser.json();
 
 app.post("/login", bodyJSON, (req, res)=> {
     console.log(req.body)
--- example-3/products.js
+++ example-3/products.js
@@ -1,15 +1,18 @@
 const products = [
     {
         _id: "2tfsfdfd",
-        name: "Lenovo X120S"
+        name: "Lenovo X120S",
+        brand: "ThinkPad"
     },
     {
         _id: "54ffhh75",
-        name: "Samsung Note 4"
+        name: "Samsung Note 4",
+        brand: "Note"
     },
     {
         _id: "g648744d",
-        name: "iPhone X"
+        name: "iPhone X",
+        brand: "iPhone"
     }
 ];
 
